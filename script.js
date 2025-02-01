bbadacc2daefa44ebb38837875b2c8a3f9fea23b
Qiwie84 <emirizaguirre8425@gmail.com>
2025-02-01 12:42:19 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+        // Reproducir la música al hacer clic en la página
+        document.body.addEventListener('click', function() {
+            var audio = document.getElementById('audio');
+            audio.play();
+        });
