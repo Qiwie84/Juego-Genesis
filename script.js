493a913ad09a0436baeb6540000c59e7e1878a00
Qiwie84 <emirizaguirre8425@gmail.com>
2025-02-01 13:03:25 -0600

    Delete script.js

--- script.js
+++
@@ -1,5 +0,0 @@
-        // Reproducir la música al hacer clic en la página
-        document.body.addEventListener('click', function() {
-            var audio = document.getElementById('audio');
-            audio.play();
-        });
